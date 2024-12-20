520c29a7dfa7964e396ddfc1d9c9214adb9c056a
toniseldr <17658949-toniseldr@users.noreply.replit.com>
2024-12-20 13:09:51 +0000

    Agent query: Is the application now loading without any React hook errors? Can you see the Start Brewing button?
    Move BrewingProvider to main.tsx to fix Invalid hook call error.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/e8ddb50d-9b23-41df-ae0b-0e69a283738b/935f8e8e-0508-4747-ab0a-680df16adc68.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import { Switch, Route } from "wouter";
-import { BrewingProvider } from "@/context/BrewingContext";
 import BrewingPage from "@/pages/Brewing";
 
 function App() {
   return (
-    <BrewingProvider>
-      <Switch>
-        <Route path="/" component={BrewingPage} />
-      </Switch>
-    </BrewingProvider>
+    <Switch>
+      <Route path="/" component={BrewingPage} />
+    </Switch>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
+import { BrewingProvider } from "@/context/BrewingContext";
 import App from './App';
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
+      <BrewingProvider>
+        <App />
+        <Toaster />
+      </BrewingProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
